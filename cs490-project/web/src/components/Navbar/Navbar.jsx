d5027f792f838c9bfe9db3f7406168107fe60b28
Adriana Morales <89936921+amm33@users.noreply.github.com>
2024-02-21 21:33:13 -0500

    Added navbar component

---
+++ cs490-project/web/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h2>{'Navbar'}</h2>
+      <p>{'Find me in ./web/src/components/Navbar/Navbar.jsx'}</p>
+    </div>
+  )
+}
+
+export default Navbar
