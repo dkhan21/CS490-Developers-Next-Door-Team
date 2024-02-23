e6d201dac8ca558af2cd847b17d589ea753f01a0
Adriana Morales <89936921+amm33@users.noreply.github.com>
2024-02-22 21:44:42 -0500

    added material ui and links (doesn't work yet)

--- cs490-project/web/src/components/Navbar/Navbar.jsx
+++ cs490-project/web/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,107 @@
+// Import necessary packages
+import * as React from 'react'; 
+import { Link, routes } from '@redwoodjs/router'
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 const Navbar = () => {
   return (
-    <div>
-      <h2>{'Navbar'}</h2>
-      <p>{'Find me in ./web/src/components/Navbar/Navbar.jsx'}</p>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Code Harbor
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+            {/* <Link to={routes.}> */}
+              <Button color="inherit" sx={{ 
+                color: 'black',
+                position: 'relative',
+                '&:after': {
+                  content: '""',
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: '1px',
+                  backgroundColor: 'currentColor',
+                  transform: 'scaleX(0)',
+                  transformOrigin: 'left',
+                  transition: 'transform 0.5s ease-in-out',
+                },
+                '&:hover:after': {
+                  transform: 'scaleX(1)',
+                },
+              }}>About</Button>
+            {/* </Link> */}
+
+            {/* <Link to={routes.}> */}
+              <Button color="inherit" sx={{ 
+                color: 'black',
+                position: 'relative',
+                '&:after': {
+                  content: '""',
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: '1px',
+                  backgroundColor: 'currentColor',
+                  transform: 'scaleX(0)',
+                  transformOrigin: 'left',
+                  transition: 'transform 0.5s ease-in-out',
+                },
+                '&:hover:after': {
+                  transform: 'scaleX(1)',
+                },
+              }}>Get Started</Button>
+            {/* </Link> */}
+
+            {/* <Link to={routes.}> */}
+              <Button color="inherit" sx={{ 
+                color: 'black',
+                position: 'relative',
+                '&:after': {
+                  content: '""',
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  height: '1px',
+                  backgroundColor: 'currentColor',
+                  transform: 'scaleX(0)',
+                  transformOrigin: 'left',
+                  transition: 'transform 0.5s ease-in-out',
+                },
+                '&:hover:after': {
+                  transform: 'scaleX(1)',
+                },
+              }}>Login</Button>
+            {/* </Link> */}
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+
   )
 }
 
--- cs490-project/web/src/pages/HomePage/HomePage.jsx
+++ cs490-project/web/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+import Navbar from 'src/components/Navbar/Navbar'
 
 const HomePage = () => {
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
+      <Navbar/>
       <header>
         <h1>HomePage</h1>
         <nav></nav>
