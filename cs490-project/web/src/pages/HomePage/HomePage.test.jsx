72a832e916896f4a95a38c77ebccf6360d946070
richytran <rtran3102@gmail.com>
2024-03-05 11:35:10 -0500

    added tests

--- cs490-project/web/src/pages/HomePage/HomePage.test.jsx
+++ cs490-project/web/src/pages/HomePage/HomePage.test.jsx
@@ -1,14 +1,55 @@
-import { render } from '@redwoodjs/testing/web'
+import { screen } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 
 import HomePage from './HomePage'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//   https://redwoodjs.com/docs/testing#testing-pages-layouts
-
 describe('HomePage', () => {
-  it('renders successfully', () => {
-    expect(() => {
+  test('renders all elements correctly', () => {
+    render(<HomePage />)
+
+    expect(screen.getByText('Welcome To Code Harbor!')).toBeInTheDocument()
+    expect(
+      screen.getByText('The Place To Convert Your Code,')
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText('Into Other Programming Languages!')
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText('Paste or upload your code in the input box.')
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'From the first dropdown menu, select the programming language that code is currently in.'
+      )
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        'Next, in the second dropdown menu, select the language desired to be converted to.'
+      )
+    ).toBeInTheDocument()
+    expect(screen.getByText('Lastly, click convert!')).toBeInTheDocument()
+  })
+  describe('HomePage', () => {
+    test('renders all images correctly', () => {
       render(<HomePage />)
-    }).not.toThrow()
+
+      expect(screen.getByAltText('People on computer')).toBeInTheDocument()
+      expect(screen.getByAltText('Code on computer')).toBeInTheDocument()
+    })
+  })
+
+  describe('HomePage', () => {
+    test('navigates to the correct page when Get Started button is clicked', () => {
+      const { getByText } = render(
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      )
+
+      const button = getByText('Get Started')
+      fireEvent.click(button)
+      // expect(history.location.pathname).toBe('/start')
+    })
   })
 })
