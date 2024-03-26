50f57a3417e28b8fc14f0ac55b58e12327bc4243
khanfarooq123 <farooqakhan1118@gmail.com>
2024-03-26 07:27:57 -0400

    Added some API Tests For GPT

--- cs490-project/api/src/functions/openai/openai.test.js
+++ cs490-project/api/src/functions/openai/openai.test.js
@@ -5,19 +5,89 @@ import { handler } from './openai'
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-functions
 
+
 describe('openai function', () => {
-  it('Should respond with 200', async () => {
+  it('Authenticates and receives successful response', async () => {
+
+    const simulatedPayload = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            content: "printf('Hello');",
+            target: "Python",
+            source: "C"
+          }
+        ]
+      }),
+    };
+
+    const response = await handler(simulatedPayload, null)
+
+    const responseBody = JSON.parse(response.body) 
+
+    expect(response.statusCode).toBe(200)
+    expect(responseBody.completion).toBe("print('Hello')")
+  })
+
+  it('Unsuccessful request', async () => {
     const httpEvent = mockHttpEvent({
       queryStringParameters: {
-        id: '42', // Add parameters here
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          messages: [
+            {
+              content: "printf('Hello');",
+              target: "Python",
+              source: "C"
+            }
+          ]
+      }),
       },
     })
 
-    const response = await handler(httpEvent, null)
-    const { data } = JSON.parse(response.body)
+    const result = await handler(httpEvent)
+    const body = result.body
 
-    expect(response.statusCode).toBe(200)
-    expect(data).toBe('openai function')
+    expect(result.statusCode).toBe(500)
+  })
+
+  it('Successful request', async () => {
+
+    const dataPayload = {
+      "messages": [
+        {
+          "role": "system",
+          "content": "a",
+          "source": "b",
+          "target": "c"
+        }
+      ]
+    };
+
+
+
+
+    const httpEvent = mockHttpEvent({
+      queryStringParameters: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataPayload),
+      },
+    })
+
+    const result = await handler(httpEvent)
+    const body = result.body
+
+    expect(result.statusCode).toBe(500)
   })
 
   // You can also use scenarios to test your api functions
