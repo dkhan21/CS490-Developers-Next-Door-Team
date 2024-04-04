2a47dff42989354c068f746e864089cbec3c65d3
Adriana Morales <89936921+amm33@users.noreply.github.com>
2024-04-04 00:45:29 -0400

    Fixed up the profile page a bit more

--- cs490-project/web/src/pages/ProfilePage/ProfilePage.jsx
+++ cs490-project/web/src/pages/ProfilePage/ProfilePage.jsx
@@ -4,18 +4,19 @@ import { useEffect } from 'react'
 import { useAuth } from 'src/auth'
 import { useQuery, gql, useMutation } from '@apollo/client'
 import { toast, Toaster } from '@redwoodjs/web/toast'
-import { Button, TextField, Box, Typography } from '@mui/material';
+import { Button, TextField, Box, Typography, Grid } from '@mui/material';
 import Sidebar from 'src/components/Sidebar/Sidebar'
 import Navbar from 'src/components/Navbar/Navbar'
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const DELETE_USER_MUTATION = gql`
-  mutation DeleteUserMutation($id: Int!) {
-    deleteUser(id: $id){
-      id
-    }
-  }
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#44BBA4',
+    },
+  },
+});
 
-`
 const USER_QUERY = gql`
   query FindUserById($id: Int!){
     user: user(id: $id){
@@ -34,22 +35,6 @@ const ProfilePage = () => {
     variables: {id: currentUser?.id}
   })
 
-  const [deleteUser, { loading, error }] = useMutation(DELETE_USER_MUTATION, {
-    onCompleted: () => {
-      //show success message
-      alert('Account successfully deleted') 
-      logOut()
-      navigate('/')
-    },
-  })
-
-  const onClickDelete = () => {
-    if (confirm('Are you sure you want to delete your account?')){
-      deleteUser({ variables: { id: currentUser.id }})
-    }
-  }
-
-
   if (!currentUser){
     return <div>Loading...</div> 
   }
@@ -60,43 +45,30 @@ const ProfilePage = () => {
   }
 
   return (
-    <Box>
-      <Metadata title="Profile" description="Profile page" />
+    <>
       <Navbar/>
-
-      <div>
-        <button onClick={onClickDelete}>Delete Account</button>
+      <div style={{ display: 'flex' }}>
+      <Sidebar/>
+      <div style = {{ flex: 1, marginRight: "300px" }}>
+        <ThemeProvider theme={theme}>
+          <Grid container direction="column" alignItems="center" justifyContent="center" style={{ minHeight: '100vh'}}>
+            <Grid item xs={12} style={{ textAlign: 'center', marginBottom: "20px "}}>
+              <h1>Profile</h1>
+            </Grid>
+            <Grid item xs={12} container justifyContent="center">
+              <Grid item style={{ maxWidth: '400px', width: '100%', marginBottom: '13px' }}>
+                <Typography>Email: {currentUser.email}</Typography>
+                {data && data.user && <Typography>Name: {data.user.name} </Typography>}
+                <h3>Preferences</h3>
+                {data && data.user && <Typography>Preferred Programming Language: {data.user.preferredProgrammingLanguage}</Typography>}
+                {data && data.user && <Typography>Preferred IDE: {data.user.preferredIDE}</Typography>}
+              </Grid>
+            </Grid>
+          </Grid>
+        </ThemeProvider>
+        </div>
       </div>
-
-      <Box sx={{ display: 'flex', height: '100vh' }}>
-        <Sidebar/>
-        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%' }}> 
-          <Box sx={{ color: '#393E41' }}>
-          <Typography variant="h4">PROFILE</Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'column', '& > :not(style)': { m: 1 }}}>
-            {/* Email */}
-            <Typography>Email: {currentUser.email}</Typography>
-            {/* Name */}
-            {data && data.user && <Typography>Name: {data.user.name} </Typography>}
-
-            {/* Preferred Programming Language */}
-            <Typography>Preferences</Typography>
-            {data && data.user && <Typography>Preferred Programming Language: {data.user.preferredProgrammingLanguage}</Typography>}
-            {/* Preferred IDE */}
-            {data && data.user && <Typography>Preferred IDE: {data.user.preferredIDE}</Typography>}
-
-
-            {/* <Button variant="contained" sx={{ bgcolor: '#44BBA4', '&:hover': { bgcolor: '#E7BB41' }}}>Update Profile</Button>
-            <Button variant="contained" sx={{ bgcolor: '#44BBA4', '&:hover': { bgcolor: '#E7BB41' }}}>Change Password</Button>
-            <Button variant="contained" sx={{ bgcolor: '#44BBA4', '&:hover': { bgcolor: '#E7BB41' }}}>Delete Account</Button> */}
-          </Box>
-        </Box>
-
-
-
-        </Box>
-      </Box>
-    </Box>
+    </>
   )
   
 }
