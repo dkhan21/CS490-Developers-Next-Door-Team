3c42c4ee20638c06ec7e2d7238b3ca9c73f606a3
Adriana Morales <89936921+amm33@users.noreply.github.com>
2024-04-06 23:34:30 -0400

    Added sidebar tests

--- cs490-project/web/src/components/Sidebar/Sidebar.test.jsx
+++ cs490-project/web/src/components/Sidebar/Sidebar.test.jsx
@@ -1,9 +1,8 @@
-import { render } from '@redwoodjs/testing/web'
+import { render , screen, fireEvent} from '@redwoodjs/testing/web'
+import { MemoryRouter } from 'react-router-dom';
 
 import Sidebar from './Sidebar'
 
-//   Improve this test with help from the Redwood Testing Doc:
-//    https://redwoodjs.com/docs/testing#testing-components
 
 describe('Sidebar', () => {
   it('renders successfully', () => {
@@ -11,4 +10,54 @@ describe('Sidebar', () => {
       render(<Sidebar />)
     }).not.toThrow()
   })
+
+  it('navigates to Profile page when Profile link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/profile']}>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText('Profile'));
+
+    expect(window.location.pathname).toBe('/profile');
+  });
+
+  it('navigates to Update Profile page when Update Profile link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/update-profile']}>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText('Update Profile'));
+
+    expect(window.location.pathname).toBe('/update-profile');
+  });
+
+  it('navigates to Password page when Password link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/change-password']}>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText('Password'));
+
+    expect(window.location.pathname).toBe('/change-password');
+  });
+
+  it('navigates to Delete My Account page when Delete My Account link is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={['/delete-account']}>
+        <Sidebar />
+      </MemoryRouter>
+    );
+
+    fireEvent.click(screen.getByText('Delete My Account'));
+
+    expect(window.location.pathname).toBe('/delete-account');
+  });
+
+
 })
