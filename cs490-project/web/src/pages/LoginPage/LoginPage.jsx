99d0d8b37445f2ed2ddfabc1c5501c1f85ba230a
Daniyal Khan <123338238+dkhan21@users.noreply.github.com>
2024-04-23 02:01:41 -0400

    Update LoginPage.jsx

--- cs490-project/web/src/pages/LoginPage/LoginPage.jsx
+++ cs490-project/web/src/pages/LoginPage/LoginPage.jsx
@@ -58,7 +58,7 @@ const LoginPage = () => {
         },
       ],
     }
-    await fetch('https://codeharbordnd.netlify.app/.netlify/functions/openai', {
+    await fetch('https://codeharbordnd.netlify.app/.netlify/functions/genOneTimeKey', {
       mode: 'cors',
       method: 'POST',
       headers: {
@@ -130,7 +130,7 @@ const LoginPage = () => {
         },
       ],
     }
-    await fetch('https://codeharbordnd.netlify.app/.netlify/functions/openai', {
+    await fetch('https://codeharbordnd.netlify.app/.netlify/functions/genOneTimeKey', {
       mode: 'cors',
       method: 'POST',
       headers: {
