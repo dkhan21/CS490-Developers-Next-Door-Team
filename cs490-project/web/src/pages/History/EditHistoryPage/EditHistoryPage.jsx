d3dfe21d8e7ea8a5fb49bd550b4b888ac2c53fdd
Daniyal Khan <dkhann17@gmail.com>
2024-04-22 21:19:32 -0400

    deploy2

--- cs490-project/web/src/pages/History/EditHistoryPage/EditHistoryPage.jsx
+++
@@ -1,7 +0,0 @@
-import EditHistoryCell from 'src/components/History/EditHistoryCell'
-
-const EditHistoryPage = ({ id }) => {
-  return <EditHistoryCell id={id} />
-}
-
-export default EditHistoryPage
